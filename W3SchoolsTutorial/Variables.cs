adb93b5c65f73a9d0dead231dcb14855fe22f181
Paris Cornett <pariscornett@gmail.com>
2020-01-11 19:47:11 -0600

    Adds variables

---
+++ W3SchoolsTutorial/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace W3SchoolsTutorial
+{
+    public class Variables //this class doesn't do anything
+    {
+        public Variables()  //nothing is returned or output here
+        {
+            
+        }
+    }
+}
+
+
+//--NOTES ON VARIABLES
+// In C#, variables are defined by their data type rather than general variable keywords like "var" "let" and "const" in JavaScript
+// "int" : stores whole number integers without decimals (ex: 150, -637)
+// "double" : stores floating point numbers, aka numbers with decimals. (ex: 34.08, -5.23)
+// "char" : stores single characters, and the characters (i.e. the data values) are surrounded by single quotes (ex: 'f' or 'Y')
+// "string" : stores strings. this one is self explanatory for anyone who is familiar with a programming language. surrounded by double quotes ( "string" or "My name is Paris")
+// "bool" : stores booleans, which have two states: true or false.
+
+//--VARIABLE SYNTAX IN C#
+// Syntax is the same as with JavaScript and follows this structure:
+// type variableName = value;
+// ex: int exampleNumber = 5;
